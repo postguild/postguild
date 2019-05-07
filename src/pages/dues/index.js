3fb0fffcf724ab76c339c676007d6f984531d670
Joe Fox <joe.m.fox@gmail.com>
2019-05-06 20:22:10 -0400

    working on the dues calculator

--- src/pages/dues/index.js
+++ src/pages/dues/index.js
@@ -2,14 +2,26 @@ import React from "react";
 
 import Layout from "../../components/Layout";
 import Counter from "./calculator";
-import { graphql, StaticQuery } from "gatsby";
+// import { graphql, StaticQuery } from "gatsby";
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div>Hello, hello</div>
-        <Counter />
+        <section className="section section--gradient">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="section">
+                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    How much are my dues?
+                  </h2>
+                  <Counter />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
     );
   }
