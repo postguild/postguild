1c21629bfe06d2b69c270b44c6290cb629c487eb
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2019-05-07 17:19:05 -0400

    restyling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,45 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import logo from '../img/GuildLogoFinalAlt.png'
-import facebook from '../img/social/facebook.svg'
-import twitter from '../img/social/twitter.svg'
+import logo from "../img/GuildLogoFinalAlt.png";
+import facebook from "../img/social/facebook.svg";
+import twitter from "../img/social/twitter.svg";
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em' }}
-          />
+        <div className="columns">
+          <div className="column is-8 has-text-centered">
+            <img
+              src={logo}
+              alt="The Washington Post Guild logo"
+              style={{ width: "14em" }}
+            />
+          </div>
+
+          <div className="column is-4 social has-text-centered">
+            <a
+              title="facebook"
+              href="https://www.facebook.com/groups/WaPoGuild/"
+            >
+              <img
+                src={facebook}
+                alt="Facebook"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+            <a title="twitter" href="https://twitter.com/postguild?lang=en">
+              <img
+                className="fas fa-lg"
+                src={twitter}
+                alt="Twitter"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+          </div>
         </div>
+        {/*
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
@@ -91,9 +115,10 @@ const Footer = class extends React.Component {
             </div>
           </div>
         </div>
+        */}
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
