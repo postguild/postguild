bba2f23be86de684453c24fdd351174736625982
Joe Fox <joe.m.fox@gmail.com>
2019-05-06 22:06:45 -0400

    basic working dues calculator

--- src/pages/dues/calculator.js
+++ src/pages/dues/calculator.js
@@ -1,33 +1,26 @@
 import React from "react";
+import { Range } from "react-range";
+import "./calc.sass";
 
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.rate = 0.013846
-    let dues = 70000*this.rate
-    
+    this.rate = 0.013846;
+    let dues = 70000 * this.rate;
+
     this.state = {
       salary: 70000,
-      dues:dues
+      dues: dues
     };
 
-    this.handleChange = this.handleChange.bind(this)
-    
+    this.handleChange = this.handleChange.bind(this);
   }
-  increment(e) {
-    e.preventDefault();
-    this.setState({ cnt: this.state.cnt + 1 });
-  }
-  decrement(e) {
-    e.preventDefault();
-    this.setState({ cnt: this.state.cnt - 1 });
-  }
-  handleChange(e){
-    let dues = e.target.value*this.rate
+  handleChange(v) {
+    let dues = v[0] * this.rate;
     this.setState({
-      salary:e.target.value,
-      dues:dues
-    })
+      salary: v[0],
+      dues: dues
+    });
   }
   render() {
     let msgClass =
@@ -39,25 +32,82 @@ export default class Counter extends React.Component {
 
     return (
       <div>
-        <div className="dues-calculator">
-          <p class="explainer-text">If your annual salary is {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.state.salary).replace(/\D00$/, '')}</p>
-          <input 
-          type="range"
-          value={this.state.salary}
-          min="0"
-          max="200000"
-          step="1000"
-          onChange={this.handleChange}
-          ></input>
-          <h5 style={h5StyleOverride} className="col-lg-1 text-center">
-            You will pay {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.state.dues)} yearly.
-          </h5>
-          <h5 style={h5StyleOverride} className="col-lg-1 text-center">
-            You will pay {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.state.dues/12)} monthly.
-          </h5>
-          <h5 style={h5StyleOverride} className="col-lg-1 text-center">
-            You will pay {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.state.dues/52)} weekly.
-          </h5>
+        <div className="calculator-container">
+          <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+            How much are my dues?
+          </h2>
+          <p className="explainer-text">
+            NewsGuild members pay <em>1.3846%</em> of their salary as dues,
+            which support the larger union organization's work on our behalf.
+            This tool will give you an estimate of how much you can expect to
+            contribute through dues, which are automatically deducted from your
+            paycheck.
+          </p>
+          <h3 className="viz-label">A member with a yearly salary of</h3>
+          <Range
+            values={[this.state.salary]}
+            min={0}
+            max={200000}
+            step={1000}
+            onChange={values => {
+              this.handleChange(values);
+            }}
+            renderTrack={({ props, children }) => (
+              <div
+                className="slider-track"
+                {...props}
+                style={{
+                  ...props.style,
+                  height: "6px",
+                  width: "100%",
+                  backgroundColor: "#ccc"
+                }}
+              >
+                {children}
+              </div>
+            )}
+            renderThumb={({ props }) => (
+              <div
+                className="drag-handle"
+                {...props}
+                style={{
+                  ...props.style
+                }}
+              >
+                <div className="salary-text">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD"
+                  })
+                    .format(this.state.salary)
+                    .replace(/\D00$/, "")}
+                </div>
+              </div>
+            )}
+          />
+          <h3 className="viz-label">will pay:</h3>
+          <p className="salary-text">
+            {" "}
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD"
+            }).format(this.state.dues)}{" "}
+            yearly
+          </p>
+          <p className="salary-text">
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD"
+            }).format(this.state.dues / 12)}{" "}
+            monthly
+          </p>
+          <p className="salary-text">
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD"
+            }).format(this.state.dues / 52)}{" "}
+            weekly
+          </p>
         </div>
       </div>
     );
--- src/pages/dues/index.js
+++ src/pages/dues/index.js
@@ -13,9 +13,6 @@ export default class BlogIndexPage extends React.Component {
             <div className="columns">
               <div className="column is-10 is-offset-1">
                 <div className="section">
-                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                    How much are my dues?
-                  </h2>
                   <Counter />
                 </div>
               </div>
