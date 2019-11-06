3376342d27b5601f7a688061227614b2e2dd483f
Aaron Williams <aaron.colby.williams@gmail.com>
2019-11-06 15:58:52 -0500

    boost pay study emphasis

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,7 +67,7 @@ const Navbar = class extends React.Component {
                 Join Us
               </Link>
               <Link className="navbar-item" to="/2019-pay-study/">
-                Pay Study
+                <b>✨ Pay Study</b>
               </Link>
               <Link className="navbar-item" to="/leadership/">
                 Leadership
