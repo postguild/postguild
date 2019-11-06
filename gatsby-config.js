af0577781c0a3769d3121cd01d2ae7f0a7e43ba5
Aaron Williams <aaron.colby.williams@gmail.com>
2019-11-06 10:15:59 -0500

    create SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,12 @@ var proxy = require("http-proxy-middleware");
 module.exports = {
   siteMetadata: {
     title: "The Washington Post Guild",
+    titleTemplate: "%s | The Washington Post Guild",
     description:
-      "The Washington Post Newspaper Guild has been a voice for employees at the company since 1934. We represent 1,000 people across the newsroom and in our commercial operations — including award-winning journalists and the talented marketing, advertising and printing plant staff that help readers and watchers find their work."
+      "The Washington Post Newspaper Guild has been a voice for employees at the company since 1934. We represent 1,000 people across the newsroom and in our commercial operations — including award-winning journalists and the talented marketing, advertising and printing plant staff that help readers and watchers find their work.",
+    url: "https://postguild.com",
+    image: "/img/guildlogo-og.jpg",
+    twitterUsername: "@postguild"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
