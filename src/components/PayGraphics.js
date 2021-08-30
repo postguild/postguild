5462505d446797786f273134b248d977719e25bc
Joe Fox <joe.m.fox@gmail.com>
2021-08-29 21:25:16 -0400

    trying another deploy

--- src/components/PayGraphics.js
+++ src/components/PayGraphics.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 // import payData from "../../static/data/pay_study_int_data.csv"
+const d3 = require("d3");
 const payData = require("../../static/data/pay_study_int_data.json");
 
 export default function PayGraphics(props) {
