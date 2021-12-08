b3ba006e62074d64b14904570730c9d9a21f7b38
Joe Fox <joe.m.fox@gmail.com>
2021-12-08 18:46:36 -0500

    replace contract page content

--- src/pages/contract/index.js
+++ src/pages/contract/index.js
@@ -24,96 +24,36 @@ export default class BlogIndexPage extends React.Component {
                     Guild Contract
                   </h2>
                   <div className="content">
-                    <p>
-                      The Post Guild bargaining committee spent more than a year
-                      negotiating our current contract, which expires in June
-                      2020. It was a tough fight that yielded a solid contract,
-                      but there’s much we wanted that The Post refused to give
-                      us. We’ll do better next time — but we need your help.
-                    </p>
-                    <p>
-                      We’ve compiled a round-up of new rights and key existing
-                      ones, along with the pages where you can find them in the
-                      contract:
-                    </p>
+                    
+                    <p className="c1">Thanks to the National Labor Relations Act, <span className="bold">you have a federally protected right to join a union</span> and strive alongside colleagues for a fair, safe and equitable workplace. This law protects wearing union buttons, shirts or stickers on the job, signing petitions and asking other employees to support the union. It means you can't be fired or otherwise retaliated against for participating in Guild activities or refusing to do work you feel is dangerous.</p>
 
-                    <ul>
-                      <li>
-                        <span className="tag is-warning">NEW</span>{" "}
-                        <strong>
-                          You have the right to request a pay review from HR.
-                        </strong>{" "}
-                        Pay inequity is a pervasive issue in our industry and
-                        newsroom, so during 2018 contract negotiations the Guild
-                        fought for a clause in the contract that empowers
-                        employees to ask for information about pay scales in
-                        their departments.
-                      </li>
-                      <li>
-                        <span className="tag is-warning">NEW</span>{" "}
-                        <strong>
-                          You get a Guild-negotiated cost of living raise.
-                        </strong>{" "}
-                        Every year, Post management grants merit raises to about
-                        one-third of staffers. The pay increase in the contract
-                        guarantees that, even without a merit raise, all
-                        employees see an annual bump of at least $780. (Page 15)
-                      </li>
-                      <li>
-                        <span className="tag is-warning">NEW</span>{" "}
-                        <strong>
-                          Your parental leave benefits have fewer restrictions.
-                        </strong>{" "}
-                        Before this contract, parents who were both Post
-                        employees had to split their four weeks of paid leave.
-                        The Guild convinced The Post to eliminate that
-                        stipulation, though management refused to budge on other
-                        parental leave asks.
-                      </li>
-                      <li>
-                        <strong>
-                          Shift workers earn extra for their nighttime hours.
-                        </strong>{" "}
-                        For each shift worked that starts between 2 p.m. and
-                        9:59 p.m., employees receive $6. For each shift that
-                        begins between 10 p.m. and 5:59 a.m., employees receive
-                        $7.50. This is not automatically added to your salary.
-                        You must manually enter your relevant shifts. (Page 15)
-                      </li>
-                      <li>
-                        <strong>
-                          You are entitled to overtime pay and/or comp time.
-                        </strong>{" "}
-                        Employees earning $1,352 per week or less ($70,304 per
-                        year) are entitled to time and a half pay for all hours
-                        worked beyond 40 in a week, with some exceptions. Anyone
-                        who earns more than $1,352 per week is entitled to comp
-                        time for any day worked outside their regular weekly
-                        schedule. (Page 20)
-                      </li>
-                      <li>
-                        <strong>
-                          You have the right to opportunities for advancement.
-                        </strong>{" "}
-                        The Post must publish notices of all job openings and
-                        give first consideration to current employees. If you
-                        are not selected for a promotion or transfer, you have
-                        the right to request an explanation for the decision and
-                        guidance about whether you would be considered for such
-                        a job in the future. (Pages 17 and 18)
-                      </li>
-                    </ul>
+                    <p className="c1"><span className="bold">Being a member of the Guild means you are not alone in any conflict with your boss or the company</span>. If a manager is discriminating against you, if you are unfairly disciplined, if you are being prevented from using sick time or other benefits guaranteed by the contract — these are all situations in which the Guild can help.</p>
 
-                    <p>
-                      View the full contract{" "}
-                      <a
-                        title="PDF of the latest contract"
-                        href="http://www.wbng.org/wp-content/uploads/2016/10/Post_Guild_Contract_2018-2020_OCR_optimized1.pdf"
-                      >
-                        here (PDF)
-                      </a>
-                      .
-                    </p>
+                    <p className="c1">Below are some of the rights all Guild-covered employees should know about. The full Guild contract is available <a className="c12" href="https://www.wbng.org/wp-content/uploads/2016/10/Post_Guild_Contract_2018-2020_OCR_optimized1.pdf">here</a>.</p>
+
+                    <p className="c1"><span className="c15 bold">You have a right to union representation at investigatory or disciplinary meetings with management.</span></p>
+                    <p className="c1"><a className="c12" href="http://www.nage.org/login/assets/images/Weingarten%20Rights%20Q%20and%20A.pdf">Weingarten Rights</a> (named for a 1975 Supreme Court case) allow employees to request the presence of a steward at any meeting that could result in disciplinary measures. The steward can act as a witness, take notes and offer advice. We encourage employees to reach out to the Guild even if they are unsure whether a meeting qualifies.</p>
+
+                    <p className="c1"><span className="c15 bold">You have a right to comp time, overtime, holiday and shift pay. </span></p>
+                    <p className="c1">Most employees who earn less than $1,352 per week ($70,304 per year) are entitled to overtime pay whenever they work holidays or more than 40 hours in a week. Staffers who work evenings and overnights are also entitled to extra pay.</p>
+                    <p className="c1">People who don't qualify for overtime are entitled to comp time for days they work outside their normal schedules. "On call" days also count, if they significantly interfere with employees' ability to enjoy their days off.</p>
+                    <p className="c1"><span className="bold">Eligible employees must fill out their time sheets on Workday </span>to ensure they receive their comp time, overtime pay or night shift differential. If your manager denies your overtime or comp time, or instructs you not to mark extra hours on your timesheet, please reach out to a guild steward or <a className="c12" href="mailto:wapounion@gmail.com">wapounion@gmail.com</a>. The complete Guild guide to hours and pay is available <a href="https://postguild.org/the-post-guild-guide-to-hours-and-pay/">here</a>.</p>
+
+                    <p className="c1"><span className="c15 bold">You have a right to paid sick leave.</span></p>
+                    <p className="c1">Most employees earn up to 15 paid sick days per year for a mental or physical illness or doctor's appointments, or to care for a sick family member. You are not obligated to tell your boss why you are taking a sick day. <span className="bold">Your boss cannot require you to "make up" hours of work missed while you are on sick leave</span>. Nor can they hold your sick leave against you — for example, denying a pay raise because of an absence for a medical treatment. Employees should enter any sick leave they take on their time sheets.</p>
+
+                    <p className="c1"><span className="c15 bold">You have a right to a pay review.</span></p>
+                    <p className="c1">Employees who believe that they are being underpaid based on race, gender, religion or another aspect of their identity can initiate a pay equity review conducted by the Human Resources Department. HR will compare your salary to those of people doing similar jobs within and outside the company, then share findings with the employee and management. Trained guild members can assist you with the request and walk you through the steps. Contact <a className="c12" href="mailto:wapounion@gmail.com">wapounion@gmail.com</a> for help.</p>
+
+                    <p className="c1"><span className="c15 bold">You have a right to object to unfair treatment.</span></p>
+                    <p className="c1">If you feel you have been subject to unfair discipline or a contract violation — such as denial of overtime pay — you have 10 working days to file a grievance through the Guild. (So please contact the Guild as soon as possible!) The Guild will present the company with a written notice and arrange for a meeting at which you and Guild representatives can discuss the issue with management. <span className="bold">These meetings can force the company to reverse disciplinary actions or make reparations for unfair treatment.</span> Contact <a className="c12" href="mailto:sarahkaplan48@gmail.com">sarahkaplan48@gmail.com</a> if you want to file a grievance.</p>
+
+                    <p className="c1"><span className="c15 bold">You have the right to opportunities for professional advancement.</span></p>
+                    <p className="c1">Current employees have the right to first consideration for all job openings. If you are not selected for a job, you have the right to ask for an explanation of the company's choice and how you can prepare to be a better candidate in the future.</p>
+
+                    <p className="c1"><span className="c8 bold">You have the right to fair and equal treatment.</span></p>
+                    <p className="c1">If you believe you are being discriminated against based on regardless of race, sex, disability, religious affiliation or other protected status, please contact the Guild. In cases where groups of people are subject to discrimination — say, sections in which employees of color are consistently underpaid — the Guild can organize a grievance on the entire group's behalf. Remember that strength comes from solidarity and numbers; <span className="bold">our greatest power to make The Post a better, fairer workplace is in our capacity to act as a group.</span></p>
+
                   </div>
                 </div>
               </div>
